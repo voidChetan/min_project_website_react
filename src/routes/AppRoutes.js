22ae2092a504f1c3849c62b558e486c3f774edd9
Vishwajit8288 <vickyrathod8288@gmail.com>
2024-02-21 14:24:49 +0530

    AppRoutes Added

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../Components/UI/Header';
-
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+import ProjectList from '../pages/ProjectList'
+import Category from '../pages/Category'
+import ContactUs from '../pages/ContactUs'
+import QueryList from '../pages/QueryList'
 const AppRoutes = () => {
 
     return (
         <div>
             <BrowserRouter>
-            <Header></Header>
-            <Routes>
-                {/* <Route path='/Home' element={<Home></Home>}></Route> */}
-            </Routes>
+                <Header></Header>
+                <Routes>
+                    <Route path='' element={<Login></Login>}></Route>
+                    <Route path='/Home' element={<Home></Home>}></Route>
+                    <Route path='/Project' element={<ProjectList></ProjectList>}></Route>
+                    <Route path='/Category' element={<Category></Category>}></Route>
+                    <Route path='/ContactUs' element={<ContactUs></ContactUs>}></Route>
+                    <Route path='/Query' element={<QueryList></QueryList>}></Route>
+                </Routes>
 
             </BrowserRouter>
-           
+
 
         </div>
     );
