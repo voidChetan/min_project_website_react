13dfad0e9132ad3860e2f9bd2f1d7da1b39a9612
Ravina Patil <ravinapatil194@gmail.com>
2024-02-21 16:06:58 +0530

    Update Constants.js

---
+++ src/services/Constants.js
@@ -0,0 +1,8 @@
+export const GETPROJECTS_FORMAINPAGE = "GetProjectsForMainPage"
+export const GETALL_PROJECTS ="GetAllProjects"
+export const GETALL_CATEGORIES = "GetAllProjectCategories"
+export const CREATE_NEW_PROJECT="CreateNewProject"
+export const DELETE_PROJECT_BY_ID= "DeleteProjectById"
+export const UPDATE_PROJECT="UpdateProject"
+export const ADD_UPDATE_CATEGORY ="AddUpdateCategory"
+export const DELETE_CATEGORY_BY_ID= "DeleteCategoryById"
