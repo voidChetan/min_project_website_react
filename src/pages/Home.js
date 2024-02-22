66aed1dc23f139158a091c7b6cb68191d19b044c
Ravina Patil <ravinapatil194@gmail.com>
2024-02-22 17:46:02 +0530

    Your descriptive commit message here

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from './Slider';
 import '../assets/CSS/slider.css';
+import { GetProjectsForMainPage } from '../services/ProjectService';
+import ProjectCard from './ProjectCard';
 
 
 const Home = () => {
+    const [itemlist, steItemlist] = useState([])
+    useEffect(() => {
+        GetProjectsForMainPage().then((data) => {
+            steItemlist(data);
+        })
+
+    }, [])
     return (
         <div>
-           <Slider />
+            <Slider />
+            <div className="col-lg-12 main-page mt-2">
+                <h4>Top <em>Trending</em> Project<em> Ideas</em></h4>
+            </div>
+            <ProjectCard itemlist={itemlist}></ProjectCard>
         </div>
     );
 };
